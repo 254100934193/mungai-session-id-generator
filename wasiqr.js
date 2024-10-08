03185151ff33f2150f65c1a748c64ff24b8867af
Yobih <160126805+254100934193@users.noreply.github.com>
2024-10-08 09:40:08 +0300

    MUNGAI

--- wasiqr.js
+++ wasiqr.js
@@ -60,7 +60,7 @@ router.get('/', async (req, res) => {
 	
 				   let WASI_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃Yobih-bot SESSION IS 
+┃Yobih-Bug_Bot SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
