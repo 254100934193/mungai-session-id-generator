c137f4f7750d76373cf44cd06bd9e94abeb8f4c4
Yobih <160126805+254100934193@users.noreply.github.com>
2024-10-08 07:56:52 +0300

    MUNGAI

--- pair.js
+++ pair.js
@@ -57,21 +57,23 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by WASI TECH*
+*_Session Connected By Yobih-Bug-Bot*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
-║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+║ *『AMAZING YOU'VE SUCCESSFULLY CONNECTED YOBIH BUG BOT』*
+║ _You Have Completed the First Step to Deploy a Yobih bug bot by Mungai._
+
 ╚════════════════════════╝
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Youtube:* _https://youtube.com/@mungaibrian-o9x?si=_2f4ab5Au88YAssV
+║❒ *Owner:* _https://wa.me/254743436105_
+║❒ *Repo:* _https://github.com/254100934193/Yobih-Bug-Bot_
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029Vajp1HY4o7qSc0kE9o1g_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vajp1HY4o7qSc0kE9o1g_
+║❒ *Plugins:* _https://github.com/254100934193/Yobih-Bug-Bot
+
 ╚════════════════════════╝
 _____________________________________
 
